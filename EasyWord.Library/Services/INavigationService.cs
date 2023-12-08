5664c73d45fa8141c69997ba659ec6ba588a57f4
某 裴 <1104952939@qq.com>
2023-12-08 11:19:48 +0800

    Revert "优化INavigation"
    This reverts commit b9461b071d0e6eee2a948673c0bc3e75f7447403.

--- EasyWord.Library/Services/INavigationService.cs
+++ EasyWord.Library/Services/INavigationService.cs
@@ -1,13 +1,13 @@
 ﻿namespace EasyWord.Library.Services;
 
-//导航Service，用于页面跳转 xj实现.
+//导航Service，用于页面跳转 xj实现
 public interface INavigationService {
     void NavigateTo(string uri);
 
     void NavigateTo(string uri, object parameter);
 }
 
-//导航页面常量.
+//导航页面常量
 public static class NavigationServiceConstants {
     public const string BooksPage = "/books";
     public const string TodayWordsPage = "/todayWords";
